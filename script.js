84afbe156d56a54946c4fa4929cddbf2bcec1f65
gongmyeong-201219 <gongmyeong201219@gmail.com>
2024-06-30 23:03:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -83,14 +83,14 @@ class Slider {
     this.textures = null;
 
     // Initialize touch variables
-    this.touchStartY = 0;
-    this.touchEndY = 0;
+    this.touchStartX = 0;
+    this.touchEndX = 0;
 
     this.init();
   }
 
   bindAll() {
-    ['render', 'nextSlide', 'prevSlide', 'handleTouchStart', 'handleTouchMove', 'handleTouchEnd'].
+    ['render', 'nextSlide', 'handleTouchStart', 'handleTouchMove', 'handleTouchEnd'].
     forEach(fn => this[fn] = this[fn].bind(this));
   }
 
@@ -226,12 +226,22 @@ class Slider {
     const currentBulletLine = currentBullet.querySelectorAll('.js-slider-bullet__line');
     const nextBulletLine = nextBullet.querySelectorAll('.js-slider-bullet__line');
 
-    const tl = new TimelineMax();
+    const tl = new TimelineMax({ paused: true });
+
+    if (this.state.initial) {
+      TweenMax.to('.js-scroll', 1.5, {
+        yPercent: 100,
+        alpha: 0,
+        ease: Power4.easeInOut });
+
+
+      this.state.initial = false;
+    }
 
     tl.
     staggerFromTo(currentImages, 1.5, {
       yPercent: 0,
-      scaleY: 1 },
+      scale: 1 },
     {
       yPercent: -185,
       scaleY: 1.5,
@@ -249,7 +259,7 @@ class Slider {
       ease: Expo.easeInOut },
     0);
 
-    if (this.state.text) {
+    if (currentText) {
       tl.
       fromTo(currentText, 2, {
         yPercent: 0 },
@@ -302,7 +312,7 @@ class Slider {
   }
 
   prevSlide() {
-    // Add logic for previous slide if needed
+
   }
 
   nextSlide() {
@@ -322,29 +332,38 @@ class Slider {
   }
 
   handleTouchStart(event) {
-    this.touchStartY = event.changedTouches[0].screenY;
+    this.touchStartX = event.changedTouches[0].screenX;
   }
 
   handleTouchMove(event) {
-    event.preventDefault(); // Prevent default touch move behavior to disable scrolling
-    this.touchEndY = event.changedTouches[0].screenY;
+    this.touchEndX = event.changedTouches[0].screenX;
   }
 
   handleTouchEnd() {
-    const swipeThreshold = 50; // Adjust this value to set the swipe sensitivity
-    if (this.touchEndY < this.touchStartY - swipeThreshold) {
+    if (this.touchEndX < this.touchStartX) {
       this.nextSlide();
     }
-    if (this.touchEndY > this.touchStartY + swipeThreshold) {
-      this.prevSlide();
-    }
+    // Optionally, you can add a previous slide functionality
+    // if (this.touchEndX > this.touchStartX) {
+    //   this.prevSlide();
+    // }
   }
 
   listeners() {
     window.addEventListener('wheel', this.nextSlide, { passive: true });
+    // Add touch event listeners to the entire slider
     this.el.addEventListener('touchstart', this.handleTouchStart, { passive: true });
-    this.el.addEventListener('touchmove', this.handleTouchMove, { passive: false });
+    this.el.addEventListener('touchmove', this.handleTouchMove, { passive: true });
     this.el.addEventListener('touchend', this.handleTouchEnd, { passive: true });
+    // Add touch event listeners to the images
+    this.slides.forEach(slide => {
+      const images = slide.querySelectorAll('.js-slide__img');
+      images.forEach(img => {
+        img.addEventListener('touchstart', this.handleTouchStart, { passive: true });
+        img.addEventListener('touchmove', this.handleTouchMove, { passive: true });
+        img.addEventListener('touchend', this.handleTouchEnd, { passive: true });
+      });
+    });
   }
 
   render() {
